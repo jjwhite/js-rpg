62177b4852d5507a0daf757da4a72ac20d6cacfd
Jon White <jjwhite01@gmail.com>
2016-07-24 08:20:43 -0400

    fleshing out character

--- js/rpg/Character.js
+++ js/rpg/Character.js
@@ -1,12 +1,38 @@
-﻿Rpg.Character = function (_name, _type) {
+﻿Rpg.Character = function (_name, _type, _max_hp, _max_mp) {
     var self = this;
 
     var Public = {
         name: _name,
         type: _type,
+        max_hp: _max_hp !== null ? _max_hp : 0, // neutral npc
+        max_mp: _max_mp !== null ? _max_mp : 0, // neutral 
+
+        // mutable status properties
+        status: {
+            life_status: Rpg.Character.Life_Status.Alive,
+            hp: _max_hp,
+            mp: _max_mp,
+        },
 
         AddWeapon: function (_weapon) {
             this.weapon = _weapon;
+        },
+
+        TakeDamage: function (_damage) {
+            this.status.hp -= _damage;
+            CheckLifeStatus();
+        }
+
+    }
+
+    var CheckLifeStatus = function () {
+        if (Public.status.hp <= 0) {
+            Public.status.hp = 0;
+            if (Public.type == Rpg.Character.Type.Hero) {
+                Public.status.life_status = Rpg.Character.Life_Status.KOed;
+            } else {
+                Public.status.life_status = Rpg.Character.Life_Status.Dead;
+            }
         }
     }
 
@@ -17,4 +43,11 @@ Rpg.Character.Type = {
     Hero: 0,
     Neutral: 1,
     Enemy: 2
+}
+
+Rpg.Character.Life_Status = {
+    Alive: 0,
+    KOed: 1,
+    Dead: 2,
+    Undead: 3,
 }
--- js/tests/Character.spec.js
+++ js/tests/Character.spec.js
@@ -2,19 +2,40 @@
 
 describe('Character component', function () {
 
-    var testChar = new Rpg.Character("test", 0);
+    
 
     it('is named test', function () {
+        var testChar = new Rpg.Character("test", Rpg.Character.Type.Hero, 100, 0);
         expect(testChar.name).toEqual("test");
     });
 
     it('is of type hero', function () {
+        var testChar = new Rpg.Character("test", Rpg.Character.Type.Hero, 100, 0);
         expect(testChar.type).toEqual(Rpg.Character.Type.Hero);
     });
 
     it('has a weapon after adding one', function () {
+        var testChar = new Rpg.Character("test", Rpg.Character.Type.Hero, 100, 0);
         testChar.AddWeapon(new Rpg.Weapon('test-weapon', 1, 10, 10));
         expect(testChar.weapon.name).toEqual('test-weapon');
+    });
+
+    it('has lower hp after taking damage', function () {
+        var testChar = new Rpg.Character("test", Rpg.Character.Type.Hero, 100, 0);
+        testChar.TakeDamage(50);
+        expect(testChar.status.hp).toEqual(50);
+    });
+
+    it('Hero is KOed after taking too much damage', function(){
+        var testChar = new Rpg.Character("test", Rpg.Character.Type.Hero, 100, 0);
+        testChar.TakeDamage(100);
+        expect(testChar.status.life_status).toEqual(Rpg.Character.Life_Status.KOed);
+    });
+
+    it('Enemy is Dead after taking too much damage', function () {
+        var testChar = new Rpg.Character("test", Rpg.Character.Type.Enemy, 100, 0);
+        testChar.TakeDamage(100);
+        expect(testChar.status.life_status).toEqual(Rpg.Character.Life_Status.Dead);
     });
 
 });
